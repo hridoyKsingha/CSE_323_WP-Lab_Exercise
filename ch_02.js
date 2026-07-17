9264309716b1c473441483d67c7afab64eab536a
Hridoy Kumar Singha <200100645+hridoyKsingha@users.noreply.github.com>
2026-07-18 04:27:55 +0600

    completed my second exercise

--- ch_02.js
+++ ch_02.js
@@ -1,7 +1,17 @@
 //1
-
 const name = "Hridoy Kumar Singha";
 const department = "CSE";
 let semester = "10th";
 
 console.log(`My name is ${name}. I study in the department of ${department}. Now I am continuing my ${semester} semester`);
+
+
+//2
+console.log(typeof null);
+
+
+//3
+let numberString = "19.5";
+let number = parseFloat(numberString);
+
+console.log(number + 0.5);

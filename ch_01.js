067534d6018d42f8453629a08aeb62f5e4d010b2
Hridoy Kumar Singha <200100645+hridoyKsingha@users.noreply.github.com>
2026-07-18 04:22:23 +0600

    completed my first exercise

--- ch_01.js
+++ ch_01.js
@@ -1,2 +1,23 @@
 // 1
+console.log("Hridoy Kumar Singha");
 
+
+//2
+//welcome message
+console.log("Welcome to learn JavaScript; Web Programming Lab");
+
+
+//3
+//remove quotation mark from console.log
+
+console.log("Hridoy Kumar Singha");
+
+    // SyntaxError: Invalid or unexpected token
+    // at wrapSafe (node:internal/modules/cjs/loader:1804:18)
+    // at Module._compile (node:internal/modules/cjs/loader:1845:20)
+    // at Object..js (node:internal/modules/cjs/loader:2002:10)
+    // at Module.load (node:internal/modules/cjs/loader:1594:32)
+    // at Module._load (node:internal/modules/cjs/loader:1396:12)
+    // at wrapModuleLoad (node:internal/modules/cjs/loader:255:19)
+    // at Module.executeUserEntryPoint [as runMain] (node:internal/modules/run_main:154:5)
+    // at node:internal/main/run_main_module:33:47
---
+++ ch_02.js
@@ -0,0 +1,7 @@
+//1
+
+const name = "Hridoy Kumar Singha";
+const department = "CSE";
+let semester = "10th";
+
+console.log(`My name is ${name}. I study in the department of ${department}. Now I am continuing my ${semester} semester`);

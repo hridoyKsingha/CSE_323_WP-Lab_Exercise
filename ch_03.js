d40b40d372b391a106f2157d8bd69d224af06d90
Hridoy Kumar Singha <200100645+hridoyKsingha@users.noreply.github.com>
2026-07-18 04:45:10 +0600

    completed my third/03 exercise

---
+++ ch_03.js
@@ -0,0 +1,29 @@
+//1
+// let number = -8;
+// if(number > 0){
+//     console.log("Positive");
+// }else if(number < 0){
+//     console.log("Negative");
+// }else{
+//     console.log("Zero");
+// }
+
+
+//2
+// const temperatue = 32;
+// if(temperatue > 32){
+//     console.log("Hot");
+// }else if(temperatue >=20 && temperatue <=32){
+//     console.log("Pleasent");
+// }else{
+//     console.log("Cold");
+// }
+
+//3
+
+// let number = 122;
+// const numberStatus = number%2===0? "Even" : "Odd";
+
+// console.log(numberStatus);
+
+//4

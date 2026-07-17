0a0feffe1f4dae7afe7f381c5e33c95408009f9c
Hridoy Kumar Singha <200100645+hridoyKsingha@users.noreply.github.com>
2026-07-18 04:32:34 +0600

    completed my second exercise

--- ch_02.js
+++ ch_02.js
@@ -15,3 +15,12 @@ let numberString = "19.5";
 let number = parseFloat(numberString);
 
 console.log(number + 0.5);
+
+
+//4
+
+let numStr = "5";
+let num = 3;
+
+console.log(numStr + num);   //5 is string ans 3 is number; where that's can;t addition.
+console.log(numStr - num);  //(-) not addition, then that;s worked to subtrcation.
